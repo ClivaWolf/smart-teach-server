5254b750ab0c0fd08267c0caad21164014c1235a
Cliva Wolf <cliva.wolf@mail.ru>
2024-06-14 18:47:09 +0300

    files CRUD template

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestResModule } from './test-res/test-res.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [TestResModule],
+  imports: [TestResModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
