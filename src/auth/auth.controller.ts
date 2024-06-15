4888cf2c1284da66b9c5a458601793dd84d9be65
Cliva Wolf <cliva.wolf@mail.ru>
2024-06-15 18:43:21 +0300

    auth controller swagger doc up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,27 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @HttpCode(HttpStatus.OK)
   @ApiBody({
-    type:CreateUserDto
+    type: CreateUserDto
   })
+  @ApiOperation({ summary: 'Login', description: 'method for sign in by login and password' })
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.login, signInDto.password);
   }
 
+  @ApiBody({
+    type: CreateUserDto
+  })
+  @ApiOperation({ summary: 'Register', description: 'method for sign up by login, email and password' })
   @Post('register')
   register(@Body() dto: CreateUserDto) {
     return this.authService.register(dto);
