0e6c2aa8d15580ab6dc4b1b2404176ccf89a5191
Cliva Wolf <cliva.wolf@mail.ru>
2024-06-16 19:16:25 +0300

    dto add readonly

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateRoleDto {
     @ApiProperty({ example: 'ADMIN', description: 'Role value' })
-    value: string;
+    readonly value: string;
     @ApiProperty({ example: 'Администратор', description: 'Role description' })
-    description: string;
+    readonly description: string;
 }
